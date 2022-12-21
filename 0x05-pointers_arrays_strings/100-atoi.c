146c2528a63e04e999de445aa7eca9c91c1e73c7
glow0 <trish1602095@gmail.com>
2022-12-21 06:21:26 -0800

    fixing compiler problems

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,7 @@ int _atoi(char *s)
 	temp = s;
 	num = 0;
 	sign = 1;
-	while (*temp != '\0' && (temp < '0' || *temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '-')
 			sign *= -1;
@@ -22,7 +22,7 @@ int _atoi(char *s)
 	}
 	if (*temp != '\0')
 	{
-		do {num = num * 10 + (*temp _ '0');
+		do {num = num * 10 + (*temp - '0');
 			temp++;
 		} while (*temp >= '0' && *temp <= '9');
 	}
